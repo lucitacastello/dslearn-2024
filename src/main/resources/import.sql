0461c5e28aa53bf1b4dd4e983816dd8bc16ef432
cidarosa <lucita.rosa@gmail.com>
2024-06-12 11:26:14 -0300

    Course - aula 08

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,3 +13,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES('Bootcamp HTML','https://p0.pxfuel.com/preview/956/471/686/training-course-training-online-courses-learning-royalty-free-thumbnail.jpg', 'https://th.bing.com/th/id/OIP.ToulakzKxRPdzUujaymXxwHaHa?w=173&h=180&c=7&r=0&o=5&pid=1.7');
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Java Básico', 'https://www.flickr.com/photos/oracle_images/6236966575','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4fJcMhL2PP67WJR4jrze6O-jmoQz4IHkIAA&usqp=CAU');
