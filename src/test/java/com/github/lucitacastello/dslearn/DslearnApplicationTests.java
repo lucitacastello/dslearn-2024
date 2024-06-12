a07bb96289bab97a5f27eff58c2f5242b41691b6
cidarosa <lucita.rosa@gmail.com>
2024-06-12 10:20:36 -0300

    Init project

---
+++ src/test/java/com/github/lucitacastello/dslearn/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucitacastello.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
