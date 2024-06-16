8041bef336a8595811d4f2d036209895ff9a3b5b
cidarosa <lucita.rosa@gmail.com>
2024-06-16 20:50:36 -0300

    organizando arquivo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,7 +2,6 @@ INSERT INTO tb_user (name, email, password) VALUES ('Alex Brown', 'alex@gmail.co
 INSERT INTO tb_user (name, email, password) VALUES ('Bob Brown', 'bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 INSERT INTO tb_user (name, email, password) VALUES ('Maria Green', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 
-
 INSERT INTO tb_role (authority) VALUES ('ROLE_STUDENT');
 INSERT INTO tb_role (authority) VALUES ('ROLE_INSTRUCTOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
